2a126e0f64e5674523bf0e4e7378aad4d59c8776
shopid-dev <vladimirbooithsek@gmail.com>
2022-07-27 21:24:50 +0430

    add apiConfig param

--- src/components/EditObj.vue
+++ src/components/EditObj.vue
@@ -13,15 +13,21 @@ export default {
     var editobjmain = this;
 
     var dest = editobjmain.$options.apiHost + '/' + editobjmain.$options.name;
+    
+    var apiconfig = null;
+    
+    if (typeof editobjmain.$options.apiConfig != "undefined") {
+      apiconfig = editobjmain.$options.apiConfig;
+    }
 
 
       editobjmain.item.save = async function (e) {
       if (editobjmain.item.id > 0) {
-        await axios.patch(dest+"/"+editobjmain.item.id, editobjmain.item).then(response=>{
+        await axios.patch(dest+"/"+editobjmain.item.id, editobjmain.item,apiconfig).then(response=>{
            e.onSaved(this,response);
         });
       } else {
-        await axios.post(dest, editobjmain.item).then(response=>{
+        await axios.post(dest, editobjmain.item,apiconfig).then(response=>{
             
             editobjmain.item.id = response.data[editobjmain.$options.name].id;
             e.onSaved(this,response);
@@ -34,7 +40,7 @@ export default {
     };
 
     editobjmain.item.delete = async function (e) {
-    await axios.delete(dest+"/"+editobjmain.item.id, editobjmain.item).then(response=>{
+    await axios.delete(dest+"/"+editobjmain.item.id, editobjmain.item,apiconfig).then(response=>{
           
            e.onDeleted(this,response);
            
